bc818a8df7214880ba73fd21a211d451263bf7d8
ylanigisc <yves@iterate.ch>
2015-01-08 12:23:10 +0100

    Update duck formula

--- duck.rb
+++ duck.rb
@@ -1,8 +1,10 @@
+require "formula"
+
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-4.6.2.16310.tar.gz"
-  version "4.6.2.16310"
-  sha1 "df498504eb69a94237b09ea6cdfbe989aaa50416"
+  url "https://dist.duck.sh/duck-4.6.2.16313.tar.gz"
+  version "4.6.2.16313"
+  sha1 "5750c155fee25c2d015c0e5e427ee13caa7fe5b2"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
