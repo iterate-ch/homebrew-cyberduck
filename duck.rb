64cae8e9d0a49c771f109f452ad560beedcfffd8
ylanigisc <yves@iterate.ch>
2015-01-12 15:43:41 +0100

    Update formula

--- duck.rb
+++ duck.rb
@@ -1,7 +1,7 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-4.6.2.16366.tar.gz"
-  sha1 "12f19fcf28be1226bf747f529339acc14bdebc42"
+  url "https://dist.duck.sh/duck-4.6.2.16379.tar.gz"
+  sha1 "bf01b14388bded1bd42e096d81adb7d6f6f52c96"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
@@ -10,7 +10,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck 4.6.2 (16366)\n".eql? %x(#{bin}/duck -version)
+    unless "Cyberduck 4.6.2 (16379)\n".eql? %x(#{bin}/duck -version)
       fail "Version mismatch"
     end
     filename = (testpath/"test")
