9b3cb84c9d7e7a29a4e53c4bf9a604c63854afef
Jenkins CI <noreply@cyberduck.io>
2015-02-12 19:09:26 +0100

    Update formula (16776)

--- duck.rb
+++ duck.rb
@@ -1,7 +1,7 @@
 class Duck < Formula
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/duck-4.7.16752.tar.gz"
-  sha1 "4dd746e0f70b1272412bbb5eb8913f29bf347b4d"
+  url "https://dist.duck.sh/duck-4.7.16776.tar.gz"
+  sha1 "88eb6b4d91b13eca1af2a3b88835cacddc672ea4"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
@@ -10,7 +10,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck 4.7 (16752)\n".eql? %x(#{bin}/duck -version)
+    unless "Cyberduck 4.7 (16776)\n".eql? %x(#{bin}/duck -version)
       fail "Version mismatch"
     end
     filename = (testpath/"test")
