398603c814f526910acbfd43f1a6bd4661a04ce0
Jenkins CI <noreply@cyberduck.io>
2015-10-11 19:07:04 +0200

    Update formula (18304)

--- duck.rb
+++ duck.rb
@@ -2,7 +2,7 @@ class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
   url "https://dist.duck.sh/duck-4.8.18304.tar.gz"
-  sha1 "a92a4c3cb444a672c648f962d022d8882aff160c"
+  sha1 "627345f32a40f4926906b4cf71bc5cdcb5e824d5"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
