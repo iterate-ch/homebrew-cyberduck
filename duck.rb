556527580f5572803edb906fd77a72b25ef6d726
Jenkins CI <noreply@cyberduck.io>
2017-07-21 15:06:24 +0200

    Update version

--- duck.rb
+++ duck.rb
@@ -1,8 +1,8 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/nightly/duck-6.1.1.25692.tar.gz"
-  sha256 "b8615ff262b568e897b12384de21ca699de5ebf57f4d268c1572be94dae58f9c"
+  url "https://dist.duck.sh/nightly/duck-6.1.1.25729.tar.gz"
+  sha256 "0c3b81e2bad19e39826fd5265123eceefdc2862412a8ecf19932014f379c883b"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
@@ -11,7 +11,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck 6.1.1 (25692)\n".eql? %x(`#{bin}/duck -version`)
+    unless "Cyberduck 6.1.1 (25729)\n".eql? %x(`#{bin}/duck -version`)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
