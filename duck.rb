385de034323d43a2012c27bdd8039694c6d09b79
Jenkins CI <noreply@cyberduck.io>
2026-07-24 15:01:19 +0200

    Update version

--- duck.rb
+++ duck.rb
@@ -1,8 +1,8 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/nightly/duck-9.5.3.45409.tar.gz"
-  sha256 "2b66abdc566316a1cecb076764a8ee9eca0094cb7fbe610b31e6cb3c05db2fb0"
+  url "https://dist.duck.sh/nightly/duck-9.5.3.45411.tar.gz"
+  sha256 "f00681b26aa106437a945a3e755775cf01726700d6f24e4fb4432b32f4e74d56"
   license "GPL-3.0-only"
 
   depends_on "openjdk@17"
@@ -16,7 +16,7 @@ class Duck < Formula
   end
 
   test do
-    unless "Cyberduck 9.5.3 (45409)\n".eql? %x(`#{bin}/duck -version`)
+    unless "Cyberduck 9.5.3 (45411)\n".eql? %x(`#{bin}/duck -version`)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
