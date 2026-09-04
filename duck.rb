f9256ef3c3da80ca2c765ad08a04f8c7b5599670
dkocher <71733+dkocher@users.noreply.github.com>
2026-09-04 14:07:13 +0000

    Update version

--- duck.rb
+++ duck.rb
@@ -1,22 +1,22 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/nightly/duck-9.6.0.45560.tar.gz"
-  sha256 "e341331d4b1c6e1dcce7d9788240cf9375546d520b6a694ca40e44c98fcd0bb7"
+  url "https://dist.duck.sh/nightly/duck-9.6.0.45667.tar.gz"
+  sha256 "2766416469b1e336281f72085bd4f256853adbe64a4363c33e0dedc6bce672aa"
   license "GPL-3.0-only"
 
-  depends_on "openjdk@17"
+  depends_on "openjdk@21"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
     libexec.install Dir["*"]
     rm_r "#{libexec}/Contents/PlugIns/Runtime.jre"
-    ln_s Formula["openjdk@17"].libexec/"openjdk.jdk", "#{libexec}/Contents/PlugIns/Runtime.jre"
+    ln_s Formula["openjdk@21"].libexec/"openjdk.jdk", "#{libexec}/Contents/PlugIns/Runtime.jre"
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
 
   test do
-    unless "Cyberduck 9.6.0 (45560)\n".eql? %x(`#{bin}/duck -version`)
+    unless "Cyberduck 9.6.0 (45667)\n".eql? %x(`#{bin}/duck -version`)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
