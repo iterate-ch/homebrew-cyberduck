f4c259d7406057254ec30b0523113ec2cd6bb6ef
Jenkins CI <noreply@cyberduck.io>
2026-09-06 15:01:29 +0200

    Update version

--- duck.rb
+++ duck.rb
@@ -1,22 +1,22 @@
 class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
-  url "https://dist.duck.sh/nightly/duck-9.6.0.45669.tar.gz"
-  sha256 "b58c81bf90ca26c16ea05b911c704b85d2b50057ddfb800e639c265cd794f27d"
+  url "https://dist.duck.sh/nightly/duck-9.6.0.45563.tar.gz"
+  sha256 "b4d3426625aa6c2e925ff26a1e7a84915ebf04286953a2b61ae99ca32c54f1f3"
   license "GPL-3.0-only"
 
-  depends_on "openjdk@21"
+  depends_on "openjdk@17"
 
   def install
     # Because compiling would need a JDK and xcodebuild we just use the pre-compiled binary.
     libexec.install Dir["*"]
     rm_r "#{libexec}/Contents/PlugIns/Runtime.jre"
-    ln_s Formula["openjdk@21"].libexec/"openjdk.jdk", "#{libexec}/Contents/PlugIns/Runtime.jre"
+    ln_s Formula["openjdk@17"].libexec/"openjdk.jdk", "#{libexec}/Contents/PlugIns/Runtime.jre"
     bin.install_symlink "#{libexec}/Contents/MacOS/duck" => "duck"
   end
 
   test do
-    unless "Cyberduck 9.6.0 (45669)\n".eql? %x(`#{bin}/duck -version`)
+    unless "Cyberduck 9.6.0 (45563)\n".eql? %x(`#{bin}/duck -version`)
       raise "Version mismatch"
     end
     filename = (testpath/"test")
